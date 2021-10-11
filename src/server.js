c91c2ab37089b85c13c3fa62caed0fae1de083a0
syjeon <jsyu3799@gmail.com>
2021-10-11 23:43:59 +0900

    response status, download 테스트

--- src/server.js
+++ src/server.js
@@ -12,8 +12,18 @@ const handleLogin = (req, res) => {
     return res.send("this page is login page🍗")
 }
 
+const handleAdmin = (req, res) => {
+    return res.status(404).send("sorry, we haven't made this page yet😅")
+}
+
+const handleDownload = (req, res) => {
+    return res.download("/Users/syjeon/Downloads/empty.txt")
+}
+
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 👑`);
 
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/admin", handleAdmin);
+app.get("/download", handleDownload);
 app.listen(PORT, handleListening);
