9d6c0d30b193c1bf78d5232345c7a02cc75685d6
syjeon <jsyu3799@gmail.com>
2021-10-11 22:55:42 +0900

    res.send를 통한 요청 응답

--- src/server.js
+++ src/server.js
@@ -4,7 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleListening = () =>
-    console.log(`✅ Server listening on port http://localhost:${PORT} 👑`);
+const handleHome = (req, res) => {
+    return res.send("i love you forever");
+};
 
+const handleLogin = (req, res) => {
+    return res.send("this page is login page🍗")
+}
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 👑`);
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 app.listen(PORT, handleListening);
