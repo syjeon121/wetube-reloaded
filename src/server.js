83d4e4491c38abfd78b68ba88736ff8cf965a3c7
syjeon <jsyu3799@gmail.com>
2021-10-19 17:29:29 +0900

    불필요한 내용 삭제

--- src/server.js
+++ src/server.js
@@ -4,26 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-    return res.send("i love you forever");
+const handleHome = (req, res, next) => {
+    return res.send({ message: "i love you forever" });
 };
-
-const handleLogin = (req, res) => {
-    return res.send("this page is login page🍗")
-}
-
-const handleAdmin = (req, res) => {
-    return res.status(404).send("sorry, we haven't made this page yet😅")
-}
-
-const handleDownload = (req, res) => {
-    return res.download("/Users/syjeon/Downloads/empty.txt")
-}
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 👑`);
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+app.use(logger);
+
 app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/admin", handleAdmin);
-app.get("/download", handleDownload);
 app.listen(PORT, handleListening);
